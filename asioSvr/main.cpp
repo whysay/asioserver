b36535e2c8ffa9342ef01c25a1271ba6b911931d
whysay <gal_yong@naver.com>
2014-08-04 13:37:21 +0900

    ga

--- asioSvr/main.cpp
+++ asioSvr/main.cpp
@@ -7,7 +7,7 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
-// 111
+// 1
 #include <cstdlib>
 #include <iostream>
 #include <memory>
